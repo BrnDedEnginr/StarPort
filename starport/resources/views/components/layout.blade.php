a947e551bab9aee86894c1f4496f7e8f3e7076f4
BrnDedEnginr <167724484+BrnDedEnginr@users.noreply.github.com>
2025-03-21 01:33:15 +0100

    Added daisyui, layout and navbar

---
+++ starport/resources/views/components/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>StarPort - {{$title}}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="drawer">
+        <input id="nav-drawer" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content">
+            <x-navbar />
+            {{ $slot }}
+        </div>
+        <div class="drawer-side">
+            <label for="nav-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
+            <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                <!--
+                <li><a>Sidebar Item 1</a></li>
+                <li><a>Sidebar Item 2</a></li>
+                -->
+            </ul>
+        </div>
+    </div>
+
+</body>
+
+</html>
